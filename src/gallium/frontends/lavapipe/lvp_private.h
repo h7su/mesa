2a1ae3b4266c4fe443c85a49d52b16aef1575725
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2022-04-28 16:43:30 -0400

    lavapipe: include drm_fourcc.h
    Reviewed-by: Dave Airlie <airlied@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16224>

--- src/gallium/frontends/lavapipe/lvp_private.h
+++ src/gallium/frontends/lavapipe/lvp_private.h
@@ -44,6 +44,10 @@
 #include "cso_cache/cso_context.h"
 #include "nir.h"
 
+#if !DETECT_OS_WINDOWS
+#include "drm-uapi/drm_fourcc.h"
+#endif
+
 /* Pre-declarations needed for WSI entrypoints */
 struct wl_surface;
 struct wl_display;
