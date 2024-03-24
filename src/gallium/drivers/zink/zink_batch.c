40a47e623d31a9b49d7121a3b79558f8b1e97eed
Lina Versace <linyaa@google.com>
2024-03-24 15:04:42 -0700

    zink: Do not submit main cmdbuf if it lacks work
    I believe more command buffers can be eliminated, specifically those that
    do unneeded barriers (vkCmdPipelineBarrier). But this patch eliminates
    the large majority of unneeded command buffers in the workload
    I analyzed.
    
    On an apitrace of Unturned on Intel RaptorLake, this MR reduced the
    number of VkCommandBuffer submissions from ~17k to ~12k (reduction 0.71x).
    
    Issue: https://gitlab.freedesktop.org/mesa/mesa/-/issues/10813

--- src/gallium/drivers/zink/zink_batch.c
+++ src/gallium/drivers/zink/zink_batch.c
@@ -687,7 +687,8 @@ submit_queue(void *data, void *gdata, int thread_index)
       cmdbufs[c++] = bs->unsynchronized_cmdbuf.vk;
    if (bs->reordered_cmdbuf.has_work)
       cmdbufs[c++] = bs->reordered_cmdbuf.vk;
-   cmdbufs[c++] = bs->main_cmdbuf.vk;
+   if (bs->main_cmdbuf.has_work)
+      cmdbufs[c++] = bs->main_cmdbuf.vk;
    si[ZINK_SUBMIT_CMDBUF].pCommandBuffers = cmdbufs;
    si[ZINK_SUBMIT_CMDBUF].commandBufferCount = c;
    /* assorted signal submit from wsi/externals */
@@ -714,14 +715,16 @@ submit_queue(void *data, void *gdata, int thread_index)
 
 
    VkResult result;
-   VRAM_ALLOC_LOOP(result,
-      VKSCR(EndCommandBuffer)(bs->main_cmdbuf.vk),
-      if (result != VK_SUCCESS) {
-         mesa_loge("ZINK: vkEndCommandBuffer failed (%s)", vk_Result_to_str(result));
-         bs->is_device_lost = true;
-         goto end;
-      }
-   );
+   if (bs->main_cmdbuf.has_work) {
+      VRAM_ALLOC_LOOP(result,
+         VKSCR(EndCommandBuffer)(bs->main_cmdbuf.vk),
+         if (result != VK_SUCCESS) {
+            mesa_loge("ZINK: vkEndCommandBuffer failed (%s)", vk_Result_to_str(result));
+            bs->is_device_lost = true;
+            goto end;
+         }
+      );
+   }
    if (bs->reordered_cmdbuf.has_work) {
       if (bs->unordered_write_access) {
          VkMemoryBarrier mb;
