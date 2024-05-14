0d98a226c25a181641b04be0dba3263d37e953e1
Rhys Perry <pendingchaos02@gmail.com>
2024-05-14 18:34:01 +0100

    aco: use ac_hw_cache_flags
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>

--- src/amd/compiler/aco_print_ir.cpp
+++ src/amd/compiler/aco_print_ir.cpp
@@ -262,6 +262,20 @@ print_sync(memory_sync_info sync, FILE* output)
       print_scope(sync.scope, output);
 }
 
+template <typename T>
+static void
+print_cache_flags(enum amd_gfx_level gfx_level, const T& instr, FILE* output)
+{
+   if (instr.cache.value & ac_glc)
+      fprintf(output, " glc");
+   if (instr.cache.value & ac_slc)
+      fprintf(output, " slc");
+   if (instr.cache.value & ac_dlc)
+      fprintf(output, " dlc");
+   if (instr.cache.value & ac_swizzled)
+      fprintf(output, " swizzled");
+}
+
 static void
 print_instr_format_specific(enum amd_gfx_level gfx_level, const Instruction* instr, FILE* output)
 {
@@ -413,12 +427,7 @@ print_instr_format_specific(enum amd_gfx_level gfx_level, const Instruction* ins
    }
    case Format::SMEM: {
       const SMEM_instruction& smem = instr->smem();
-      if (smem.glc)
-         fprintf(output, " glc");
-      if (smem.dlc)
-         fprintf(output, " dlc");
-      if (smem.nv)
-         fprintf(output, " nv");
+      print_cache_flags(gfx_level, smem, output);
       print_sync(smem.sync, output);
       break;
    }
@@ -467,12 +476,7 @@ print_instr_format_specific(enum amd_gfx_level gfx_level, const Instruction* ins
          fprintf(output, " idxen");
       if (mubuf.addr64)
          fprintf(output, " addr64");
-      if (mubuf.glc)
-         fprintf(output, " glc");
-      if (mubuf.dlc)
-         fprintf(output, " dlc");
-      if (mubuf.slc)
-         fprintf(output, " slc");
+      print_cache_flags(gfx_level, mubuf, output);
       if (mubuf.tfe)
          fprintf(output, " tfe");
       if (mubuf.lds)
@@ -502,12 +506,7 @@ print_instr_format_specific(enum amd_gfx_level gfx_level, const Instruction* ins
       }
       if (mimg.unrm)
          fprintf(output, " unrm");
-      if (mimg.glc)
-         fprintf(output, " glc");
-      if (mimg.dlc)
-         fprintf(output, " dlc");
-      if (mimg.slc)
-         fprintf(output, " slc");
+      print_cache_flags(gfx_level, mimg, output);
       if (mimg.tfe)
          fprintf(output, " tfe");
       if (mimg.da)
@@ -579,12 +578,7 @@ print_instr_format_specific(enum amd_gfx_level gfx_level, const Instruction* ins
       const FLAT_instruction& flat = instr->flatlike();
       if (flat.offset)
          fprintf(output, " offset:%d", flat.offset);
-      if (flat.glc)
-         fprintf(output, " glc");
-      if (flat.dlc)
-         fprintf(output, " dlc");
-      if (flat.slc)
-         fprintf(output, " slc");
+      print_cache_flags(gfx_level, flat, output);
       if (flat.lds)
          fprintf(output, " lds");
       if (flat.nv)
@@ -631,12 +625,7 @@ print_instr_format_specific(enum amd_gfx_level gfx_level, const Instruction* ins
          fprintf(output, " offen");
       if (mtbuf.idxen)
          fprintf(output, " idxen");
-      if (mtbuf.glc)
-         fprintf(output, " glc");
-      if (mtbuf.dlc)
-         fprintf(output, " dlc");
-      if (mtbuf.slc)
-         fprintf(output, " slc");
+      print_cache_flags(gfx_level, mtbuf, output);
       if (mtbuf.tfe)
          fprintf(output, " tfe");
       if (mtbuf.disable_wqm)
