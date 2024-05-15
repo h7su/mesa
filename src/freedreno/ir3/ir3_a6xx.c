aerisarn/mesa-uwp

commit 60686d4146cd3856dd2066f6e49560b1dd76bd66

ir3/a6xx: fix ldg/stg of ulong2 and ulong4 data
Partially revert the commit f4c9e9329cf ("ir3/a6xx: Fix immediate
offset stg/ldg path").

There is no need to multiply the immediate offsets by 4. Doing so
results in loading and/or storing the data at wrong locations.

Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26817>

src/freedreno/ir3/ir3_a6xx.c
@@ -343,7 +343,7 @@ emit_intrinsic_load_global_ir3(struct ir3_context *ctx,
                                  nir_src_as_int(intr->src[1]) > -(1 << 10);
 
    if (const_offset_in_bounds) {
-      load = ir3_LDG(b, addr, 0, create_immed(b, nir_src_as_int(intr->src[1]) * 4),
+      load = ir3_LDG(b, addr, 0, create_immed(b, nir_src_as_int(intr->src[1])),
                      0, create_immed(b, dest_components), 0);
    } else {
       offset = ir3_get_src(ctx, &intr->src[1])[0];
@@ -386,7 +386,7 @@ emit_intrinsic_store_global_ir3(struct ir3_context *ctx,
 
    if (const_offset_in_bounds) {
       stg = ir3_STG(b, addr, 0,
-                    create_immed(b, nir_src_as_int(intr->src[2]) * 4), 0,
+                    create_immed(b, nir_src_as_int(intr->src[2])), 0,
                     value, 0,
                     create_immed(b, ncomp), 0);
    } else {

h7su/mesa

commit 538bf528032041298ef6da280f33e02b35e0cbed

freedreno/ir3: Fix ldg/stg offset
Basically a revert (but not a clean one) of commit 60686d4146cd
("ir3/a6xx: fix ldg/stg of ulong2 and ulong4 data").  The offset
is a byte offset, not a dword offset.

Backport note: Prior to commit 513fa1873cf5 ("ir3/a7xx: Fix
load_global_ir3 with immediate offset") you could instead just revert
the original commit.

Fixes: 60686d4146cd ir3/a6xx: ("fix ldg/stg of ulong2 and ulong4 data")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/11169
Signed-off-by: Rob Clark <robdclark@chromium.org>

src/freedreno/ir3/ir3_a6xx.c
@@ -346,7 +346,7 @@ emit_intrinsic_load_global_ir3(struct ir3_context *ctx,
 
    if (const_offset_in_bounds) {
       load = ir3_LDG(b, addr, 0,
-                     create_immed(b, nir_src_as_int(intr->src[1]) << shift),
+                     create_immed(b, (nir_src_as_int(intr->src[1]) * 4) << shift),
                      0, create_immed(b, dest_components), 0);
    } else {
       offset = ir3_get_src(ctx, &intr->src[1])[0];
@@ -389,7 +389,7 @@ emit_intrinsic_store_global_ir3(struct ir3_context *ctx,
 
    if (const_offset_in_bounds) {
       stg = ir3_STG(b, addr, 0,
-                    create_immed(b, nir_src_as_int(intr->src[2])), 0,
+                    create_immed(b, nir_src_as_int(intr->src[2]) * 4), 0,
                     value, 0,
                     create_immed(b, ncomp), 0);
    } else {